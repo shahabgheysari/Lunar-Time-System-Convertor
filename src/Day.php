165afc098a7434136688250a17475ecdc2cf0b38
Shahab Gheysari <ShahabGheysari@gmail.com>
2021-09-11 01:38:49 +0430

    added guard for day constructor and refactored and wrote tests

--- src/Day.php
+++ src/Day.php
@@ -4,12 +4,32 @@
 namespace LST;
 
 
+use InvalidArgumentException;
+
 class Day
 {
 
     private $initialDay = 1;
+    private int $value;
+
+    /**
+     * Day constructor.
+     * @param int $days
+     */
+    public function __construct(int $days)
+    {
+        if ($days <=0 || $days > 12)
+            throw new InvalidArgumentException('day is out of range');
+        $this->value = $days;
+    }
+
     public function getInitialDay()
     {
         return $this->initialDay;
+    }
+
+    public function toTerrestrialSeconds()
+    {
+        return (new Cycle(30))->toTerrestrialSeconds() * $this->value;
     }
 }
--- src/tests/DayTest.php
+++ src/tests/DayTest.php
@@ -4,13 +4,31 @@
 namespace LST\tests;
 
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use LST\Day;
 class DayTest extends TestCase
 {
+    public function testZeroDayExceptionWillBeThrown()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Day(0);
+    }
+
+    public function test13DaysExceptionWillBeThrown()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Day(13);
+    }
     public function testInitialDay()
     {
-        $day = new Day();
+        $day = new Day(1);
         $this->assertEquals(1,$day->getInitialDay());
+    }
+
+    public function testTwoDaysToTerrestrialSeconds()
+    {
+        $oneDay = new Day(2);
+        $this->assertEquals(5102885.7792022464,$oneDay->toTerrestrialSeconds());
     }
 }
