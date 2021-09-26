7674595d87ca0fcdda0f26775fb42af5134237f5
Shahab Gheysari <ShahabGheysari@gmail.com>
2021-09-26 13:34:31 +0330

    logged exception messages,refactored handling errors in controller and returned proper message on exception occurrence

--- app/Http/Controllers/LSTController.php
+++ app/Http/Controllers/LSTController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\InputModel;
 use App\Services\LSTService;
 use Illuminate\Http\Request;
-use InvalidArgumentException;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 
@@ -27,28 +28,74 @@ class LSTController extends Controller
 
     public function convert(Request $request)
     {
-        $data = '';
-        $message='';
-        $statusCode = 200;
+        $result = [
+            'data' => '',
+            'message' => '',
+            'statusCode' => 200
+        ];
+        $errorMessage = 'check your input, correct format is = yyyy-mm-dd hh:mm:ss and it should be greater than 1969-07-21 02:56:15!!';
+
         try {
-            $this->validateDateTime($request->input('dateTime'));
-            $data = $this->LSTService->convertUTCDateTime($request->input('dateTime'));
+            if ($this->validateInput($request)) {
+                $result['data'] = $this->LSTService->convertUTCDateTime(new InputModel($request->json('dateTime')));
+            }else{
+                $result['message'] = $errorMessage;
+            }
         }catch (Throwable $exception){
-            $statusCode = 400;
-            $message = 'check your input, correct format = yyyy-mm-dd hh:mm:ss and it should be greater than 1969-07-21 02:56:15!!';
+            $result['statusCode'] = 400;
+            Log::error($exception->getMessage());
+            $result['message'] = $errorMessage;
         }
 
         return $this->jsonResponse([
-            'data' => $data,
-            'message' => $message
-        ],$statusCode);
+            'data' => $result['data'],
+            'message' => $result['message']
+        ],$result['statusCode']);
     }
 
-    private function validateDateTime(string $dateTime)
+    private function validateInput(Request $request)
     {
+        $dateTime = $request->input('dateTime');
+        if ($this->inputIsNotEmpty($dateTime) &&
+            $this->requestIsJson($request) &&
+            $this->checkInputUTCFormat($dateTime)) {
+            return true;
+        }
+        return false;
+    }
 
-        if (empty(trim($dateTime)) || !preg_match("/^\d\d\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) (00|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $dateTime)) {
-            throw new InvalidArgumentException('incorrect date time format.');
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    private function requestIsJson(Request $request): bool
+    {
+        if (!$request->isJson()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param $dateTime
+     * @return bool
+     */
+    private function inputIsNotEmpty($dateTime): bool
+    {
+        if (empty(trim($dateTime))) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param $dateTime
+     * @return bool
+     */
+    private function checkInputUTCFormat($dateTime): bool
+    {
+        if (!preg_match("/^\d\d\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) (00|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $dateTime)){
+            return false;
         }
         return true;
     }
